7fdeff81faea5a108ec9d05072a4659664a1d958
Ckala62rus <agr.akyla@mail.ru>
2024-08-03 10:30:25 +0300

    (feature) improved user model.

--- src/domains/user.go
+++ src/domains/user.go
@@ -1,6 +1,12 @@
 package domains
 
+import "time"
+
 type User struct {
-	Name string
-	Age  int
+	Id        int    `gorm:"not null;uniqueIndex;primary_key"`
+	Name      string `gorm:"size:100;not null;default:null"`
+	Email     string `gorm:"unique;size:100;not null;uniqueIndex;default:null"`
+	Password  string `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
