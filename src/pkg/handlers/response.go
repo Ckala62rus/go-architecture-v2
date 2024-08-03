be73cc1fc92a855d7200f11e838d7cc9f0115e16
Ckala62rus <agr.akyla@mail.ru>
2024-08-03 10:31:10 +0300

    (feature) improved Error response.go

--- src/pkg/handlers/response.go
+++ src/pkg/handlers/response.go
@@ -3,8 +3,9 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 type ErrorResponse struct {
-	Message string `json:"message"`
-	Status  bool   `json:"status"`
+	Message string      `json:"message"`
+	Status  bool        `json:"status"`
+	Data    interface{} `json:"data"`
 }
 
 type StatusResponse struct {
@@ -14,5 +15,5 @@ type StatusResponse struct {
 }
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
-	c.AbortWithStatusJSON(statusCode, ErrorResponse{message, false})
+	c.AbortWithStatusJSON(statusCode, ErrorResponse{message, false, nil})
 }
