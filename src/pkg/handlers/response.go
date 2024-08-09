952673772ebcfe6e39f5001754f022eb8a18639d
Ckala62rus <agr.akyla@mail.ru>
2024-08-09 09:28:51 +0300

    (feature) improved custom response and error response struct.

--- src/pkg/handlers/response.go
+++ src/pkg/handlers/response.go
@@ -3,8 +3,8 @@ package handlers
 import "github.com/gin-gonic/gin"
 
 type ErrorResponse struct {
-	Message string      `json:"message"`
-	Status  bool        `json:"status"`
+	Message string      `json:"message" default:"Explaining of error description"`
+	Status  bool        `json:"status" default:"False"`
 	Data    interface{} `json:"data"`
 }
 
@@ -15,5 +15,12 @@ type StatusResponse struct {
 }
 
 func newErrorResponse(c *gin.Context, statusCode int, message string) {
-	c.AbortWithStatusJSON(statusCode, ErrorResponse{message, false, nil})
+	c.AbortWithStatusJSON(
+		statusCode,
+		ErrorResponse{
+			message,
+			false,
+			nil,
+		},
+	)
 }
