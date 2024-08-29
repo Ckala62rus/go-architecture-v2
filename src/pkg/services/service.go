3e8ced0691992e4d23d93304fdf900d3a947d831
Ckala62rus <agr.akyla@mail.ru>
2024-08-29 19:56:49 +0300

    (feature) add authorization service.

--- src/pkg/services/service.go
+++ src/pkg/services/service.go
@@ -6,7 +6,7 @@ import (
 )
 
 type Users interface {
-	CreateUser(user domains.User) (domains.User, error)
+	//CreateUser(user domains.User) (domains.User, error)
 	GetUserByName(name string) (domains.User, error)
 	GetById(id int) (domains.User, error)
 	GetAllUsers() []domains.User
@@ -14,12 +14,20 @@ type Users interface {
 	UpdateUser(userRequest domains.User) (domains.User, error)
 }
 
+type Authorization interface {
+	CreateUser(user domains.User) (int, error)
+	GenerateToken(email, password string) (string, error)
+	ParseToken(token string) (int, error)
+}
+
 type Service struct {
 	Users
+	Authorization
 }
 
 func NewService(rep *repositories.Repository) *Service {
 	return &Service{
-		Users: NewUserService(rep.Users),
+		Users:         NewUserService(rep.Users),
+		Authorization: NewAuthService(rep.Authorization, rep.Users),
 	}
 }
