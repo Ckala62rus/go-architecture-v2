c7a6d325410caac06c56ec9eb446eb5c912844dd
Ckala62rus <agr.akyla@mail.ru>
2024-09-06 21:54:48 +0300

    (feature) added config connector.

---
+++ src/pkg/utils/config_connector.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"practice/internal/config"
+)
+
+var (
+	MainConfig *config.Config
+)
+
+func init() {
+	projectDir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("***************** CONFIG RUN *****************")
+	MainConfig = config.MustLoad(filepath.Join(projectDir, "config", "local.yml"))
+}
