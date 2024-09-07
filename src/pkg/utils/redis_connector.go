f4cc4248151b40805842e854cf17f7cd3e85cb95
Ckala62rus <agr.akyla@mail.ru>
2024-09-07 13:51:58 +0300

    (feature) added method get for redis_connector.go

--- src/pkg/utils/redis_connector.go
+++ src/pkg/utils/redis_connector.go
@@ -2,10 +2,23 @@ package utils
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"github.com/redis/go-redis/v9"
 	"time"
 )
+
+var (
+	RedisDb *RedisCache
+)
+
+func init() {
+	host := MainConfig.RedisConfig.Host
+	port := MainConfig.RedisConfig.Port
+	db := MainConfig.RedisConfig.Db
+	fmt.Println("***************** REDIS RUN *****************")
+	RedisDb = NewRedisCache(host+":"+port, db)
+}
 
 type JWTCache interface {
 	SetToken(token string) *redis.StatusCmd
@@ -44,5 +57,37 @@ func (cache RedisCache) SetToken(
 		}
 	}(rbd)
 	res := rbd.Set(ctx, "user:"+token, token, time.Second*30)
+	fmt.Println(res)
 	return res
+}
+
+func (cache RedisCache) DeleteToken(
+	ctx context.Context,
+	token string,
+) error {
+	rbd := cache.getClient()
+	iter := rbd.Scan(ctx, 0, "user:"+token, 0).Iterator()
+	for iter.Next(ctx) {
+		rbd.Del(ctx, iter.Val())
+	}
+	if err := iter.Err(); err != nil {
+		return errors.New(err.Error())
+	}
+	return nil
+}
+
+func (cache RedisCache) GetToken(
+	ctx context.Context,
+	token string,
+) (string, error) {
+	rbd := RedisDb.getClient()
+	res := rbd.Get(ctx, "user:"+token)
+
+	err := res.Err()
+	if err != nil {
+		return "", err
+	}
+
+	tokenRedis := res.Val()
+	return tokenRedis, nil
 }
